0e8f7a18939d705e00dc85f0d1e7025915245b90
itmaniax <contact@itmaniax.com>
2019-04-22 13:57:10 +0000

    BE - GDrive - Backup every day and monthly scripts

--- backups/mysql/daily_backup.sh
+++ backups/mysql/daily_backup.sh
@@ -9,7 +9,7 @@ DIR="$(cd "$(dirname "$0")" && pwd -P)"
 DATE=`date '+%Y-%m-%d-%H-%M-%S'`
 TODAY_YMD=`date +%Y-%m-%d`
 STORAGE=$DIR/storage
-FILE=$STORAGE/$TODAY_YMD.sql
+FILE=$STORAGE/${TODAY_YMD}_daily.sql
 TARGZ=$FILE.tar.gz
 PAST_15_DAYS=$(date --date="${TODAY_YMD} - 15 day" +%s)
 COUNTER=0
@@ -21,14 +21,14 @@ source $DIR/../../.env;
 # create file
 docker exec $DB_CONTAINER /usr/bin/mysqldump -u $DB_ROOT_USER --password=$DB_ROOT_PASS $DB_DATABASE > $FILE
 
-tar -cvzf $STORAGE/$TODAY_YMD.sql.tar.gz $STORAGE/$TODAY_YMD.sql
+tar -cvzf $STORAGE/${TODAY_YMD}_daily.sql.tar.gz $FILE
 
 rm -f $FILE
 
 echo `/usr/local/bin/gdrive upload $TARGZ --parent $GDRIVE_DIR_BACKUP_ID`
 
 # delete files
-for i in `ls -pt $STORAGE/*.sql.tar.gz | tail -n+10`;
+for i in `ls -pt $STORAGE/*.sql.tar.gz`;
     do
         if [ -f $i ] ; then
             rm $i
---
+++ backups/mysql/monthly_backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# gdrive configuration
+#http://olivermarshall.net/how-to-upload-a-file-to-google-drive-from-the-command-line/
+
+# dir where script is
+DIR="$(cd "$(dirname "$0")" && pwd -P)"
+
+DATE=`date '+%Y-%m-%d-%H-%M-%S'`
+TODAY_YMD=`date +%Y-%m-%d`
+STORAGE=$DIR/storage
+FILE=$STORAGE/${TODAY_YMD}_monthly.sql
+TARGZ=$FILE.tar.gz
+
+# source .env file
+source $DIR/../../.env;
+
+# create file
+docker exec $DB_CONTAINER /usr/bin/mysqldump -u $DB_ROOT_USER --password=$DB_ROOT_PASS $DB_DATABASE > $FILE
+
+tar -cvzf $STORAGE/${TODAY_YMD}_monthly.sql.tar.gz $FILE
+
+rm -f $FILE
+
+echo `/usr/local/bin/gdrive upload $TARGZ --parent $GDRIVE_DIR_BACKUP_ID`
+
+# delete files
+for i in `ls -pt $STORAGE/*.sql.tar.gz`;
+    do
+        if [ -f $i ] ; then
+            rm $i
+        fi
+done;

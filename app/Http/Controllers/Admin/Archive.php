2141f18387effd2f46d7ca1d63c686458e7aeada
Kis Robert <robert.kis.itm@gmail.com>
2020-03-27 17:36:29 +0200

    BE - Automatic Publish - Publish No Tips

--- app/Http/Controllers/Admin/Archive.php
+++ app/Http/Controllers/Admin/Archive.php
@@ -54,7 +54,7 @@ class Archive extends Controller
 
             // noTip not have results and status
             if (!$distribution->isNoTip && !($distribution->statusId && $distribution->statusId == '4' ) ) {
-                if (!$distribution->result || !$distribution->statusId) {
+                if ((!$distribution->result || !$distribution->statusId) && !$distribution->isNoTip) {
                     $notHaveResultOrStatus++;
                     continue;
                 }
